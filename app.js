c5868ba474b42be6a6a2023ff17a8c19ac1783cd
Luiz <luizpatricio1309@gmail.com>
2025-10-30 19:43:01 -0300

    localizações

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
+//sorveteria souza
 var marker = L.marker([-28.42817146625198, -48.882797330888394]).addTo(map);
+//padaria laranjeiras
+var marker = L.marker([-28.43095320636672, -48.8714613417296]).addTo(map);
+//triunfo
+var marker = L.marker([-28.4264997252423, -48.88545773324723]).addTo(map);
+//prefeitura santiago
+var marker = L.marker([-28.41389452357522, -48.89347251510526]).addTo(map);
+//cartorio
+var marker = L.marker([-28.42636021504351, -48.88601731334215]).addTo(map);
